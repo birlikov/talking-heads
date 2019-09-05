d3501f2220aed027d128a9acde282b3922604df8
Shokhan Birlikov <birlikoff@gmail.com>
2019-09-05 15:47:54 +0600

    small changes

--- generator.py
+++ generator.py
@@ -4,6 +4,8 @@ import pickle
 from run import load_model, save_image
 
 import config
+import network
+
 
 
 parser = argparse.ArgumentParser(description='Generate frame from landmark')
@@ -12,22 +14,7 @@ parser.add_argument("--e_hat", required=True, help='path to e_hat tensor')
 parser.add_argument("--landmark", required=True, help = 'path to landmark tensor')
 args = parser.parse_args()
 
-#
-# def load_model(model, continue_id):
-#     filename = f'{type(model).__name__}_{continue_id}.pth'
-#     state_dict = torch.load(os.path.join(config.MODELS_DIR, filename))
-#     model.load_state_dict(state_dict)
-#     return model
-#
-# def save_image(filename, data):
-#     if not os.path.isdir(config.GENERATED_DIR):
-#         os.makedirs(config.GENERATED_DIR)
-#
-#     data = data.clone().detach().cpu()
-#     img = (data.numpy().transpose(1, 2, 0) * 255.0).clip(0, 255).astype("uint8")
-#     img = Image.fromarray(img)
-#     img.save(filename)
-
+G = network.Generator(GPU['Generator'])
 G = load_model(G, continue_id)
 
 x_hat = G(y_t, e_hat)
